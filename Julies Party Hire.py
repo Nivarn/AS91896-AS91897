14c210f8043b37519d6049ae6413bff1aa8f18e6
Nivarn <st22135@ormiston.school.nz>
2023-05-22 11:59:58 +1200

    Added Error Code
    Added Error code for not entering anything into the entry box, going over and under spin box values, and invalid receipt numbers.

--- Julies Party Hire.py
+++ Julies Party Hire.py
@@ -1,4 +1,55 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+
+#Empty list for customer information storage
+julies_party_hire_customers = []
+
+#Creating constants incase the user bypasses the limits
+min_item_no = 1
+max_item_no = 500
+
+#Creating Constants for Valid Receipt Numbers
+min_receipt_number = 100000000000
+max_receipt_number = 999999999999
+
+def customer_information():
+    name = customer_name_entry.get()
+    receipt_no = int(receipt_number_entry.get())
+    item = item_hire_entry.get()
+    no_of_item = int(no_item_spinbox.get())
+
+    print("Full Name of Customer: ", name)
+    print("Reciept Number: ", receipt_no)
+    print("Item being Hired: ", item)
+    print("Number of Item being Hired", no_of_item)
+
+
+
+    
+    appendlist = [name, receipt_no, item, no_of_item]
+    julies_party_hire_customers.append(appendlist)
+    #ERROR CODE
+    if name  == "":
+        messagebox.showwarning(title='Error!', message='Customer Name has not been entered.')
+    elif receipt_no == "":
+        messagebox.showwarning(title='Error!', message='Reciept Number has not been entered.')
+    elif receipt_no > max_receipt_number:
+        messagebox.showwarning(title='Error!', message='Invalid Receipt Number!')
+    elif receipt_no < min_receipt_number:
+        messagebox.showwarning(title = 'Error!', message='Invalid Receipt Number')
+    elif item == "":
+        messagebox.showwarning(title='Error!', message='Item for Hire has not been entered.')
+    elif no_of_item > max_item_no:
+        messagebox.showwarning(title='Error!', message='Too many Items!.')
+    elif no_of_item < min_item_no:
+        messagebox.showwarning(title='Error!', message='Invalid Number!')
+    else:
+        messagebox.showinfo(title='Stored!', message='Group information has been stored!')
+        
+
+
 
 #Main Window
 window = tk.Tk() 
@@ -37,3 +88,5 @@ for widget in julies_party_hire_frame.winfo_children():
     widget.grid_configure(pady=5, padx=10)
 
 window.mainloop()
+
+
