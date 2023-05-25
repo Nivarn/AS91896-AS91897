2e0d0a91d0e96b3c3dc1a0d16fcdd0e57263822f
Nivarn <st22135@ormiston.school.nz>
2023-05-25 14:46:08 +1200

    Changed the way the code recognises numbers

--- Julies Party Hire.py
+++ Julies Party Hire.py
@@ -10,14 +10,10 @@ julies_party_hire_customers = []
 min_item_no = 1
 max_item_no = 500
 
-#Creating Constants for Valid Receipt Numbers
-min_receipt_number = 100000000000
-max_receipt_number = 999999999999
-
 #Creating a command that can take information from the entry boxes and store it.
 def customer_information():
     name = customer_name_entry.get()
-    receipt_no = int(receipt_number_entry.get())
+    receipt_no = receipt_number_entry.get()
     item = item_hire_entry.get()
     no_of_item = int(no_item_spinbox.get())
 
@@ -35,14 +31,12 @@ def customer_information():
         messagebox.showwarning(title='Error!', message='Customer Name has not been entered.')
     elif receipt_no == "":
         messagebox.showwarning(title='Error!', message='Reciept Number has not been entered.')
-    elif receipt_no > max_receipt_number:
-        messagebox.showwarning(title='Error!', message='Invalid Receipt Number!')
-    elif receipt_no < min_receipt_number:
-        messagebox.showwarning(title = 'Error!', message='Invalid Receipt Number')
+    elif receipt_no.isnumeric() == False:
+        messagebox.showwarning(title='Error!', message= 'Reciept Number Invalid!')
     elif item == "":
         messagebox.showwarning(title='Error!', message='Item for Hire has not been entered.')
     elif no_of_item > max_item_no:
-        messagebox.showwarning(title='Error!', message='Too many Items!.')
+        messagebox.showwarning(title='Error!', message='Too many Items!')
     elif no_of_item < min_item_no:
         messagebox.showwarning(title='Error!', message='Invalid Number!')
     else:
@@ -81,7 +75,7 @@ no_item_spinbox.grid(column=1, row=3)
 #Creating Entry Box labels
 customer_name_entry_label = tk.Label(julies_party_hire_frame, text="Customer Name: ")
 customer_name_entry_label.grid(column=0,row=0)
-receipt_number_entry_label = tk.Label(julies_party_hire_frame, text="Receipt Number  (12 Digits): ")
+receipt_number_entry_label = tk.Label(julies_party_hire_frame, text="Receipt Number: ")
 receipt_number_entry_label.grid(column=0, row=1)
 item_hire_label= tk.Label(julies_party_hire_frame, text="Item for Hire: ")
 item_hire_label.grid(column=0, row=2)
@@ -92,10 +86,6 @@ no_item_label.grid(column=0, row=3)
 done_button = tk.Button(frame, text="Store", command=customer_information)
 done_button.pack(padx= 20, pady= 20)
 
-
-#Clear Entry Box Button
-clear_text_button = tk.Button(window, text="Clear Entry Boxes" )
-clear_text_button.pack()
 
 #Spacing the widgets out
 for widget in julies_party_hire_frame.winfo_children():
